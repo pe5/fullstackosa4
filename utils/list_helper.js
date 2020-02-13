05072e814500ea0dd66ba4e6eafc26d84937658b
pe5 <eero.perola@helsinki.fi>
2020-02-13 15:06:14 +0200

    teht 7

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,8 +1,8 @@
+const lodash = require('lodash')
+
 const dummy = (blogs) => {
     return 1
-  }
-  
-
+}
 
 const totalLikes = (blogs) => {
     const reducer = (sum, item) => {
@@ -18,8 +18,40 @@ const favoriteBlog = (blogs) => {
     return blogs[0]
 }
 
+const mostBlogs = (blogs) => {
+    const authors = lodash.chain(blogs)
+        .groupBy('author')
+        .map((value, key) => ({author: key, blogs: value})).value()
+    const nro = authors.map(auth => auth.blogs.length)
+    const most = Math.max.apply(null, nro)
+    const best = authors.filter(auth => auth.blogs.length === most)
+    const name = best[0].author
+    const ret = {
+        name: name,
+        blogs: best[0].blogs.length
+    }
+    return ret
+}
+
+const mostLikes = (blogs) => {
+    const authors = lodash.chain(blogs)
+        .groupBy('author')
+        .map((value, key) => ({author: key, blogs: value})).value()
+    const likes = authors.map(auth => totalLikes(auth.blogs))
+    const most = Math.max.apply(null, likes)
+    const best = authors.filter(auth => totalLikes(auth.blogs) === most)
+    const name = best[0].author
+    const ret = {
+        name: name,
+        likes: most
+    }
+    return ret
+}
+
 module.exports = {
     dummy,
     totalLikes,
-    favoriteBlog
+    favoriteBlog,
+    mostBlogs,
+    mostLikes
   }
